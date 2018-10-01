08db75ad259956fc64fefaac871c2a0909344560
davidmp <mamanipari@gmail.com>
2018-10-01 07:57:59 -0500

    Proyecto Modularizado

---
+++ AcademicoMVCCapas/AcademicoMVC-Core/src/main/java/pe/edu/upeu/academico/modelo/Profesor.java
@@ -0,0 +1,123 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pe.edu.upeu.academico.modelo;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+/**
+ *
+ * @author LAB_SOFTWARE-DTI
+ */
+@Entity
+@Table(name = "profesor")
+@NamedQueries({
+    @NamedQuery(name = "Profesor.findAll", query = "SELECT p FROM Profesor p")})
+public class Profesor implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "idProfesor")
+    private Integer idProfesor;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 10)
+    @Column(name = "grado")
+    private String grado;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 30)
+    @Column(name = "profesion")
+    private String profesion;
+    @JoinColumn(name = "idPersona", referencedColumnName = "idPersona")
+    @ManyToOne(optional = false)
+    private Persona idPersona;
+
+    public Profesor() {
+    }
+
+    public Profesor(Integer idProfesor) {
+        this.idProfesor = idProfesor;
+    }
+
+    public Profesor(Integer idProfesor, String grado, String profesion) {
+        this.idProfesor = idProfesor;
+        this.grado = grado;
+        this.profesion = profesion;
+    }
+
+    public Integer getIdProfesor() {
+        return idProfesor;
+    }
+
+    public void setIdProfesor(Integer idProfesor) {
+        this.idProfesor = idProfesor;
+    }
+
+    public String getGrado() {
+        return grado;
+    }
+
+    public void setGrado(String grado) {
+        this.grado = grado;
+    }
+
+    public String getProfesion() {
+        return profesion;
+    }
+
+    public void setProfesion(String profesion) {
+        this.profesion = profesion;
+    }
+
+    public Persona getIdPersona() {
+        return idPersona;
+    }
+
+    public void setIdPersona(Persona idPersona) {
+        this.idPersona = idPersona;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (idProfesor != null ? idProfesor.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Profesor)) {
+            return false;
+        }
+        Profesor other = (Profesor) object;
+        if ((this.idProfesor == null && other.idProfesor != null) || (this.idProfesor != null && !this.idProfesor.equals(other.idProfesor))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "pe.edu.upeu.academico.modelo.Profesor[ idProfesor=" + idProfesor + " ]";
+    }
+    
+}
---
+++ AcademicoMVCCapas/AcademicoMVC-Web/src/main/java/pe/edu/upeu/AcademicoMvcWebApplication.java
@@ -0,0 +1,12 @@
+package pe.edu.upeu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademicoMvcWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcademicoMvcWebApplication.class, args);
+	}
+}
