3035b05d3bad39dcc99e80b7b3ee3f388e16d162
davidmp <mamanipari@gmail.com>
2018-11-07 12:07:25 -0500

    Oracle CRUD Funcional

--- SysVentasJSTL/src/main/java/pe/com/syscenterlife/modelo/GloPersonas.java
+++ SysVentasJSTL/src/main/java/pe/com/syscenterlife/modelo/GloPersonas.java
@@ -37,7 +37,7 @@ public class GloPersonas implements Serializable {
 
     private static final long serialVersionUID = 1L;
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    //@GeneratedValue(strategy = GenerationType.IDENTITY)
     @Basic(optional = false)
     @Column(name = "idPersona")
     private Integer idPersona;
